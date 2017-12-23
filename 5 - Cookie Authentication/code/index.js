d8ae8dd77b781c1b0987e7dd5dc522871c693bf7
Matt Harrison <hi@matt-harrison.com>
2017-12-23 20:01:02 +0000

    thing

--- 5 - Cookie Authentication/code/index.js
+++ 5 - Cookie Authentication/code/index.js
@@ -1,7 +1,5 @@
 const Boom = require('boom');
-const Bcrypt = require('bcrypt');
 const Hapi = require('hapi');
-const HapiAuthCookie = require('hapi-auth-cookie');
 const Inert = require('inert');
 const Path = require('path');
 const Vision = require('vision');
@@ -23,15 +21,6 @@ const start = async () => {
 
     await server.register(Inert);
     await server.register(Vision);
-    await server.register(HapiAuthCookie);
-
-    server.auth.strategy('restricted', 'cookie', {
-        password: 'Td2sXhE4Eghk8MBA3X96hgMqd66k8r2P',
-        cookie: 'sid',
-        redirectTo: '/login',
-        isSecure: false,
-        validateFunc: (request, cookie) => ({ valid: true, credentials: cookie })
-    });
 
     server.views({
         relativeTo: Path.join(__dirname, 'templates'),
@@ -59,56 +48,10 @@ const start = async () => {
 
     server.route({
         method: 'GET',
-        path: '/login',
-        handler: function (request, h) {
-
-            return h.view('login');
-        }
-    });
-
-    server.route({
-        method: 'POST',
-        path: '/login',
-        handler: async function (request, h) {
-
-            const { username, password } = request.payload;
-
-            const user = Users[username];
-
-            if (!user) {
-                throw Boom.unauthorized();
-            }
-
-            if (!await Bcrypt.compare(password, user.password)) {
-                throw Boom.unauthorized();
-            }
-
-            request.cookieAuth.set(user);
-
-            return h.redirect('/');
-        }
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/logout',
-        handler: function (request, h) {
-
-            request.cookieAuth.clear();
-            return h.redirect('/');
-        }
-    });
-
-    server.route({
-        method: 'GET',
         path: '/restricted',
-        options: {
-            auth: 'restricted'
-        },
         handler: function (request, h) {
 
-            const user = request.auth.credentials;
-            return h.view('restricted', { user });
+            return h.view('restricted');
         }
     });
 
